8f38da1f25f60d93c537d526eeb9ef4e8ff418d3
Vupro2008 <104565448+Vupro2008@users.noreply.github.com>
2023-09-30 09:19:45 +0700

    Add files via upload

---
+++ cmd.sh
@@ -0,0 +1,3 @@
+curl -o /data/data/com.termux/files/usr/bin/dragonx https://raw.githubusercontent.com/Vupro2008/runtool/main/dragonxv3.4_enc.php
+chmod +x /data/data/com.termux/files/usr/bin/dragonx
+echo "\033[1;32m Thành Công ! Gõ \033[1;33mdragonx \033[1;32mĐể Vào Tools."
